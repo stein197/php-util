1ceab402b9f76889a3b7abe28a7a322d437e4b0b
Nail Gafarov <nil20122013@gmail.com>
2024-01-18 10:53:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php
+namespace Stein197\Util;
+
+use Error;
+use stdClass;
+use function array_key_exists;
+use function is_array;
+use function is_iterable;
+use function is_object;
+use function get_object_vars;
+use function str_split;
+use const PHP_INT_MAX;
+
+/**
+ * Iterate through iterables - strings, arrays, objects and iterables.
+ * @param string|object|iterable $var Variable to iterate through.
+ * @return iterable Iterable.
+ * ```php
+ * foreach (iterator('abc') as $i => $c);
+ * foreach (iterator(['a', 'b', 'c']) as $i => $c);
+ * foreach (iterator((object) ['a' => 1, 'b' => 2, 'c' => 3]) as $key => $val);
+ * ```
+ */
+function iterate(string | object | iterable $var): iterable {
+	return is_iterable($var) ? $var : (is_object($var) ? get_object_vars($var) : (is_array($var) ? $var : str_split($var)));
+}
+
+/**
+ * Check if array or object property exists.
+ * @param array|object $var Array or object to check for property existance.
+ * @param string $property Property name.
+ * @return bool `true` if property exists.
+ * ```php
+ * $a = ['a' => 1];
+ * $o = (object) $a;
+ * property_exists($a, 'a'); // true
+ * property_exists($a, 'b'); // false
+ * property_exists($o, 'a'); // true
+ * property_exists($o, 'b'); // false
+ * ```
+ */
+function property_exists(array | object $var, string $property): bool {
+	return is_array($var) ? array_key_exists($property, $var) : !!\property_exists($var, $property);
+}
+
+/**
+ * Get a property of an array or object.
+ * @param array|object $var Array or object to get property value from.
+ * @param string $property Property name.
+ * @return mixed Property value or null if the property does not exist.
+ * ```php
+ * property_get(['a' => 1], 'a');          // 1
+ * property_get((object) ['a' => 1], 'a'); // 1
+ * ```
+ */
+function &property_get(array | object &$var, string $property): mixed {
+	if (is_array($var))
+		if (property_exists($var, $property))
+			return $var[$property];
+		else
+			return null;
+	try {
+		return $var->{$property};
+	} catch (Error) {
+		return null;
+	}
+}
+
+/**
+ * Set a property value for an array or object.
+ * @param array|object $var Array or object to set property for.
+ * @param string $property Property name.
+ * @param mixed $value Property value.
+ * @return bool `true` if the operation is succeeded, `false` otherwise.
+ * ```php
+ * $a = [];
+ * $o = (object) $a;
+ * property_set($a, 'a', 1); // true
+ * $a; // ['a' => 1]
+ * property_set($o, 'a', 1); // true
+ * $o; // {a: 1}
+ * ```
+ */
+function property_set(array | object &$var, string $property, mixed $value): bool {
+	if (is_array($var)) {
+		$var[$property] = $value;
+		return $var[$property] === $value;
+	}
+	try {
+		$var->{$property} = $value;
+		return $var->{$property} === $value;
+	} catch (Error) {
+		return false;
+	}
+}
+
+/**
+ * Unset an array or object property.
+ * @param array|object $var Array or object to unset a property from.
+ * @param string $property Property name.
+ * @return bool `true` if the operation succeeded.
+ * ```php
+ * $a = ['a' => 1];
+ * $o = (object) $a;
+ * property_unset($a, 'a'); // true
+ * property_unset($o, 'a'); // true
+ * ```
+ */
+function property_unset(array | object &$var, string $property): bool {
+	if (is_array($var))
+		unset($var[$property]);
+	else
+		try {
+			unset($var->{$property});
+		} catch (Error) {
+			return false;
+		}
+	return !property_exists($var, $property);
+}
+
+/**
+ * Recursively transform a structure to an array.
+ * @param array|object $var Array or object to transform.
+ * @param int $depth Recursion depth. `PHP_INT_MAX` by default.
+ * @return array The structure, transformed to an array.
+ * ```php
+ * to_array((object) ['a' => (object) ['b' => 2]]); // ['a' => ['b' => 2]]
+ * ```
+ */
+function to_array(array | object $var, int $depth = PHP_INT_MAX): array {
+	return to_array_or_object('array', $var, $depth);
+}
+
+/**
+ * Recursively transform a structure to an object.
+ * @param array|object $var Array or object to transform.
+ * @param int $depth Recursion depth. `PHP_INT_MAX` by default.
+ * @return object The structure, transformed to an object.
+ * ```php
+ * to_object(['a' => ['b' => 2]]); // {a: {b: 2}}
+ * ```
+ */
+function to_object(array | object $var, int $depth = PHP_INT_MAX): object {
+	return to_array_or_object('object', $var, $depth);
+}
+
+// PRIVATE FUNCTIONS
+
+function is_array_or_object(mixed $var): bool {
+	return is_array($var) || is_object($var);
+}
+
+function to_array_or_object(string $type, array | object $var, int $depth): array | object {
+	if ($depth < 1)
+		$depth = 1;
+	$result = $type === 'array' ? [] : new stdClass;
+	$entries = is_array($var) ? $var : get_object_vars($var);
+	$nextDepth = $depth - 1;
+	foreach ($entries as $k => $v)
+		property_set($result, $k, is_array_or_object($v) && $depth > 1 ? to_array_or_object($type, $v, $nextDepth) : $v);
+	return $result;
+}
---
+++ test.php
@@ -0,0 +1,307 @@
+<?php
+namespace Stein197\Util;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class UtilTest extends TestCase {
+
+	#region iterate()
+
+	#[Test]
+	public function iterate_when_is_string(): void {
+		$this->assertEquals(['a', 'b', 'c'], [...iterate('abc')]);
+	}
+
+	#[Test]
+	public function iterate_when_is_array(): void {
+		$this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3], [...iterate(['a' => 1, 'b' => 2, 'c' => 3])]);
+	}
+
+	#[Test]
+	public function iterate_when_is_object(): void {
+		$this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3], [...iterate((object) ['a' => 1, 'b' => 2, 'c' => 3])]);
+	}
+
+	#[Test]
+	public function iterate_when_is_iterable(): void {
+		$result = [];
+		foreach (iterate($this->getIterableForIterate(3)) as $k => $v)
+			$result[$k] = $v;
+		$this->assertEquals([0, 3 => 3, 6 => 6], $result);
+	}
+
+	#[Test]
+	public function iterate_when_is_string_and_empty(): void {
+		$this->assertEmpty([...iterate('')]);
+	}
+
+	#[Test]
+	public function iterate_when_is_array_and_empty(): void {
+		$this->assertEmpty([...iterate([])]);
+	}
+
+	#[Test]
+	public function iterate_when_is_object_and_empty(): void {
+		$this->assertEmpty([...iterate((object) [])]);
+	}
+
+	#[Test]
+	public function iterate_when_is_iterable_and_empty(): void {
+		$this->assertEmpty([...iterate($this->getIterableForIterate(0))]);
+	}
+
+	#endregion
+
+	#region property_exists()
+
+	#[Test]
+	public function property_exists_should_return_true_when_is_array_and_property_exists(): void {
+		$this->assertTrue(property_exists(['a' => 1], 'a'));
+	}
+
+	#[Test]
+	public function property_exists_should_return_true_when_is_array_and_property_is_null(): void {
+		$this->assertTrue(property_exists(['a' => null], 'a'));
+	}
+
+	#[Test]
+	public function property_exists_should_return_true_when_is_object_and_property_exists(): void {
+		$this->assertTrue(property_exists((object) ['a' => 1], 'a'));
+	}
+
+	#[Test]
+	public function property_exists_should_return_true_when_is_object_and_property_is_null(): void {
+		$this->assertTrue(property_exists((object) ['a' => null], 'a'));
+	}
+
+	#[Test]
+	public function property_exists_should_return_false_when_is_array_and_property_does_not_exist(): void {
+		$this->assertFalse(property_exists([], 'a'));
+	}
+
+	#[Test]
+	public function property_exists_should_return_false_when_is_object_and_property_does_not_exist(): void {
+		$this->assertFalse(property_exists((object) [], 'a'));
+	}
+
+	#endregion
+
+	#region property_get()
+
+	#[Test]
+	public function property_get_should_return_value_when_is_array_and_property_exists(): void {
+		$var = ['a' => 1];
+		$this->assertEquals(1, property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_get_should_return_value_when_is_object_and_property_exists(): void {
+		$var = (object) ['a' => 1];
+		$this->assertEquals(1, property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_get_should_return_null_when_is_array_and_property_does_not_exist(): void {
+		$var = [];
+		$this->assertNull(property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_get_should_return_null_when_is_object_and_property_does_not_exist(): void {
+		$var = (object) [];
+		$this->assertNull(property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_get_should_return_null_when_is_object_and_property_is_private(): void {
+		$var = new class ('string') {
+			public function __construct(private readonly string $a) {}
+		};
+		$this->assertNull(property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_get_should_return_ref_when_is_array_and_property_exists(): void {
+		$var = ['a' => 1];
+		$prop = &property_get($var, 'a');
+		$prop = 10;
+		$this->assertEquals(10, $var['a']);
+	}
+
+	#[Test]
+	public function property_get_should_return_ref_when_is_object_and_property_exists(): void {
+		$var = (object) ['a' => 1];
+		$prop = &property_get($var, 'a');
+		$prop = 10;
+		$this->assertEquals(10, $var->a);
+	}
+
+	#endregion
+
+	#region property_set()
+
+	#[Test]
+	public function property_set_should_set_when_is_array(): void {
+		$var = [];
+		$this->assertTrue(property_set($var, 'a', 1));
+		$this->assertEquals(1, $var['a']);
+	}
+
+	#[Test]
+	public function property_set_should_set_when_is_object(): void {
+		$var = (object) [];
+		$this->assertTrue(property_set($var, 'a', 1));
+		$this->assertEquals(1, $var->a);
+	}
+
+	#[Test]
+	public function property_set_should_not_set_when_is_object_and_property_is_readonly(): void {
+		$var = new class ('string') {
+			public function __construct(public readonly string $a) {}
+		};
+		$this->assertFalse(property_set($var, 'a', 'str'));
+		$this->assertEquals('string', $var->a);
+	}
+
+	#[Test]
+	public function property_set_should_not_set_when_is_object_and_property_is_private(): void {
+		$var = new class ('string') {
+			public function __construct(private string $a) {}
+		};
+		$this->assertFalse(property_set($var, 'a', 'str'));
+		$this->assertNull(property_get($var, 'a'));
+	}
+
+	#[Test]
+	public function property_set_should_not_set_when_is_object_and_property_cannot_be_overriden(): void {
+		$var = new class ('string') {
+			public function __construct() {}
+			public function __get(string $name): mixed {
+				return null;
+			}
+			public function __set(string $name, mixed $value): void {}
+		};
+		$this->assertFalse(property_set($var, 'a', 'str'));
+		$this->assertNull($var->a);
+	}
+
+	#endregion
+	
+	#region property_unset()
+
+	#[Test]
+	public function property_unset_when_is_array(): void {
+		$var = ['a' => 1];
+		$this->assertTrue(property_unset($var, 'a'));
+		$this->assertEmpty($var);
+	}
+
+	#[Test]
+	public function property_unset_when_is_object(): void {
+		$var = (object) ['a' => 1];
+		$this->assertTrue(property_unset($var, 'a'));
+		$this->assertNull(@$var->a);
+	}
+
+	#[Test]
+	public function property_unset_should_do_nothing_when_is_object_and_not_possible_to_delete_property(): void {
+		$var = new class () {
+			private array $data;
+			public function __get(string $name): mixed {
+				return @$this->data[$name];
+			}
+			public function __set(string $name, mixed $value): void {
+				$this->data[$name] = $value;
+			}
+			public function __unset(string $name): void {}
+		};
+		$var->a = 1;
+		$this->assertFalse(property_unset($var, 'a'));
+		$this->assertEquals(1, $var->a);
+		$var = new class ('string') {
+			public function __construct(private readonly string $a) {}
+		};
+		$this->assertFalse(property_unset($var, 'a'));
+	}
+
+	#endregion
+
+	#region to_array()
+
+	#[Test]
+	public function to_array_should_do_nothing_when_it_is_an_array(): void {
+		$this->assertEquals(['a' => ['b' => ['c' => 3]]], to_array(['a' => ['b' => ['c' => 3]]]));
+	}
+
+	#[Test]
+	public function to_array_should_deeply_convert_objects(): void {
+		$this->assertEquals(['a' => ['b' => ['c' => 3]]], to_array((object) ['a' => (object) ['b' => (object) ['c' => 3]]]));
+	}
+
+	#[Test]
+	public function to_array_should_stop_at_specified_depth(): void {
+		$c = (object) ['c' => 3];
+		$this->assertEquals(['a' => ['b' => $c]], to_array((object) ['a' => (object) ['b' => $c]], 2));
+	}
+	
+	#[Test]
+	public function to_array_should_set_depth_to_1_when_it_is_less_than_1(): void {
+		$c = (object) ['c' => 3];
+		$b = (object) ['b' => $c];
+		$this->assertEquals(['a' => $b], to_array((object) ['a' => (object) ['b' => $c]], 0));
+	}
+
+	#endregion
+
+	#region to_object()
+
+	#[Test]
+	public function to_object_should_do_nothing_when_it_is_an_object(): void {
+		$c = (object) ['c' => 3];
+		$b = (object) ['b' => $c];
+		$a = (object) ['a' => $b];
+		$this->assertEquals($a, to_object($a));
+	}
+
+	#[Test]
+	public function to_object_should_deeply_convert_arrays(): void {
+		$var = &to_object(['a' => ['b' => ['c' => 3]]]);
+		$this->assertEquals(3, $var->a->b->c);
+	}
+
+	#[Test]
+	public function to_object_should_stop_at_specified_depth(): void {
+		$this->assertEquals(3, to_object(['a' => ['b' => ['c' => 3]]], 2)->a->b['c']);
+	}
+	
+	#[Test]
+	public function to_object_should_set_depth_to_1_when_it_is_less_than_1(): void {
+		$this->assertEquals(['b' => ['c' => 3]], to_object(['a' => ['b' => ['c' => 3]]], -1)->a);
+	}
+
+	#endregion
+
+	private function getIterableForIterate(int $n): Iterator {
+		return new class ($n) implements Iterator {
+			private int $i = 0;
+			public function __construct(private readonly int $steps) {}
+			public function current(): mixed {
+				return $this->i * $this->steps;
+			}
+			public function key(): mixed {
+				return $this->current();
+			}
+			public function next(): void {
+				$this->i++;
+			}
+			public function rewind(): void {
+				$this->i = 0;
+			}
+			public function valid(): bool {
+				return $this->i < $this->steps;
+			}
+		};
+	}
+}
